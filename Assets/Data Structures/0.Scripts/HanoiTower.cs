b96c0187141155f1a25fe33a1c6e0f2582e01bf1
mista-che <jeongmin.choi.15@gmail.com>
2025-07-09 15:49:44 +0900

    07-09 tower of hanoi start

---
+++ Assets/Data Structures/0.Scripts/HanoiTower.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+
+public class HanoiTower : MonoBehaviour
+{
+    public enum HanoiTower_DifficultyLevel { Lv1 = 3, Lv2, Lv3 }; // this makes the index start at 3, which is the # of donuts
+    public HanoiTower_DifficultyLevel hanoi_level;
+
+    public GameObject[] donut_prefabs; // 1 2 3 4 5 increasing size
+    public Rod[] rod_stacks; // left middle right
+
+    private Vector3 drop_position_left = new(-2.5f, 3f, 0);
+    private Vector3 drop_position_middle = new(0f, 3f, 0);
+    private Vector3 drop_position_right = new(2.5f, 3f, 0);
+
+    public static bool is_selected;
+    public static GameObject selected_donut;
+
+    IEnumerator Start()
+    {
+        for (int i = (int)hanoi_level - 1; i >= 0; i--)
+        {
+            GameObject donut = Instantiate(donut_prefabs[i]);
+
+            donut.transform.position = drop_position_left;
+
+            rod_stacks[0].PushDonut(donut);
+
+            yield return new WaitForSeconds(0.75f);
+        }
+    }
+}
---
+++ Assets/Data Structures/0.Scripts/Rod.cs
@@ -0,0 +1,79 @@
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class Rod : MonoBehaviour
+{
+    public enum RodType { LEFT, MIDDLE, RIGHT };
+    public RodType type;
+
+    public Stack<GameObject> rod_stack = new();
+
+    private void OnMouseDown()
+    {
+        if (!HanoiTower.is_selected)
+        {
+            HanoiTower.is_selected = true;
+            HanoiTower.selected_donut = PopDonut();
+        }
+        else
+        {
+            PushDonut(HanoiTower.selected_donut);
+        }
+    }
+    public bool CheckDonut(GameObject donut)
+    {
+        if (rod_stack.Count > 0)
+        {
+            int push_number = donut.GetComponent<Donut>().number;
+
+            GameObject peek_donut = rod_stack.Peek();
+            int peek_number = peek_donut.GetComponent<Donut>().number;
+
+            if (push_number < peek_number)
+            {
+                return true; // if current peek is bigger
+            }
+            else
+            {
+                Debug.LogError($"You can't move a donut of size {push_number} on top of a {peek_number}.");
+
+                // reset selection
+                HanoiTower.is_selected = false;
+                HanoiTower.selected_donut = null;
+
+
+                return false;
+            }
+        }
+        return true; // if empty
+    }
+
+    public void PushDonut(GameObject donut)
+    {
+        if (!CheckDonut(donut))
+        {
+            return;
+        }
+        else
+        {
+            // reset selection
+            HanoiTower.is_selected = false;
+            HanoiTower.selected_donut = null;
+
+            // move donut to self (rod)
+            donut.transform.position = transform.position + Vector3.up * 3f;
+            // donut.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+            // donut.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            // I should be okay without these as I turned off rotation in the rigidbody
+
+            rod_stack.Push(donut);
+        }
+
+    }
+
+    public GameObject PopDonut()
+    {
+        return rod_stack.Pop();
+    }
+}
